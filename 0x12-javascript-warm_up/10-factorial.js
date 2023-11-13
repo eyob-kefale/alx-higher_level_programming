ec050ab615c7145d89a6cb232d13fa3ad3a47442
Eyob Kefale <97896057+eyob-kefale@users.noreply.github.com>
2023-11-13 20:24:28 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-// a script that computes and prints a factorial
-
-function factorial (x) {
-  if ((isNaN(x)) || (x === 1)) {
-    return 1;
-  } else {
-      return x * factorial(x - 1);
-    }
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
