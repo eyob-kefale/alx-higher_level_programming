1661a830b0135ffe4cad35de567e9cdbbb7c6343
Eyob Kefale <97896057+eyob-kefale@users.noreply.github.com>
2023-11-13 20:25:27 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-//script that prints 3 lines
-
-const myVar1 = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'JavaScript is amazing';
-console.log (myVar1);
-console.log (myVar2);
-console.log (myVar3);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
